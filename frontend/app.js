b8c96ff258d4b9b5aa22e9840239e95d8a7afdfa
Drish86 <135121782+Drish86@users.noreply.github.com>
2024-01-06 08:47:34 +0100

    First commit  index.html

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,13 @@
 function fetchTasks() {
-  let q = document.getElementById("t").value;
+  let q = document.getElementById("newTaskTitle").value;
   fetch("/api" + "?q=" + q)
-    .then((t) => t.json())
-    .then((tasks) => {
-      document.querySelector("#tasks").innerHTML = "";
+    .then((newTaskTitle) => newTaskTitle.json())
+    .then((newTask) => {
+      document.querySelector("#newTask").innerHTML = "";
 
       let table = document.createElement("table");
 
-      tasks.forEach((element) => {
+      newTask.forEach((element) => {
         let row = document.createElement("tr");
         let td1 = document.createElement("td");
         td1.textContent = element.id;
@@ -17,7 +17,37 @@ function fetchTasks() {
         row.append(td1, td2);
         table.append(r);
 
-        document.querySelector("#tasks").append(table);
+        document.querySelector("#newTask").append(table);
       });
     });
 }
+//select user...
+function changePerson() {
+  let selectElement = document.getElementById("userSelect");
+  let selectedValue = selectElement.value;
+  console.log("Selected user: " + selectedValue);
+  if (selectedValue === "user1") {
+  } else if (selectedValue === "user2") {
+  } else if (selectedValue === "user3") {
+  }
+}
+
+//modal
+let modal = document.getElementById("taskDetailModal");
+let btn = document.getElementById("btnTaskDetails");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// zatvara modal kad se klikne izvan modala
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
